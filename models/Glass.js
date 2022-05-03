9aadec5f8e258bb25d87bd5bd66e0b2290920b3b
tamngoduc <ductam9977@gmail.com>
2022-05-03 23:13:53 +0700

    upload source

---
+++ models/Glass.js
@@ -0,0 +1,30 @@
+class Glass {
+  constructor(id, src, virtualImg, brand, name, color, price, description) {
+    this.id = id;
+    this.src = src;
+    this.virtualImg = virtualImg;
+    this.brand = brand;
+    this.name = name;
+    this.color = color;
+    this.price = price;
+    this.description = description;
+  }
+
+  renderGlass() {
+    const glasses = this.glasses.reduce((result, item) => {
+      return (
+        result +
+        `
+        <div class="col-4">
+            <div class="vglasses__items">
+                <img src="${item.src}">
+            </div>
+        </div>
+      `
+      );
+    }, "");
+    return glasses;
+  }
+}
+
+export default Glass;
